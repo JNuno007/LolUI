1c145d1f4350cccb75e378200fcc322d6da249ce
Vaazpro <vaaz@live.com.pt>
2018-06-22 16:54:24 +0100

    Finalized Panes - Design

--- LolUI/src/lolui/FXMLCreateNewMemberController.java
+++ LolUI/src/lolui/FXMLCreateNewMemberController.java
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  *
  * @author vaaz
  */
-public class FXMLManageTournamentInfoController implements Initializable {
+public class FXMLCreateNewMemberController implements Initializable {
 
     /**
      * Initializes the controller class.
